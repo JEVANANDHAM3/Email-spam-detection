cdcd178ba16747a318eb6e4d07e5dfa1168bcc0c
jevanandham <231501069@rajalakshmi.edu.in>
2025-10-26 09:02:53 +0530

    minor design change

--- frontend/app/page.js
+++ frontend/app/page.js
@@ -12,7 +12,7 @@ export default function Home() {
         <h1 className="text-4xl font-bold text-center mb-8">E-mail spam detection</h1>
 
         <Tabs defaultValue="decisionTree" className="bg-gray-900 rounded-lg p-4 shadow-lg ">
-          <TabsList className='flex gap-2 py-5 px-2 '>
+          <TabsList className='flex gap-2 py-5'>
             <TabsTrigger value="decisionTree" className=" text-black cursor-pointer hover:bg-blue-200 p-4 data-[state=active]:bg-blue-600 data-[state=active]:text-black transition-all">
               Decision Tree
             </TabsTrigger>
